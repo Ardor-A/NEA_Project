99a3d22ef7d6e0f432ee7caca58f94f291c42f74
Ardor-A <39521149+Ardor-A@users.noreply.github.com>
2018-11-02 13:56:42 +0000

    Upload

---
+++ Test 1/Test 1/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_1
+{
+    public partial class Form1 : Form
+    {
+        const int MAX_COL = 10;
+        const int MAX_ROW = 10;
+        private const int BOX_SIZE = 40;
+        TextBox[,] txtBox;
+
+        int space = 20;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            txtBox = new TextBox[MAX_COL, MAX_ROW];
+
+            for (int col = 0; col < MAX_COL; col++)
+            {
+                for (int row = 0; row < MAX_ROW; row++)
+                {
+
+                    txtBox[col, row] = new TextBox();
+                    //makes sure that the user can't type inside of the text boxes.
+                    txtBox[col, row].ReadOnly = true;
+                    txtBox[col, row].Name = col + ":" + row;
+                    txtBox[col, row].Text = col + ":" + row;
+                    txtBox[col, row].AutoSize = false;
+                    txtBox[col, row].Width = BOX_SIZE;
+                    txtBox[col, row].Height = BOX_SIZE;
+                    txtBox[col, row].Click += new EventHandler(TxtBoxClick);
+                }
+
+            }
+
+            int y = 5;
+            for (int col = 0; col < MAX_COL; col++)
+            {
+                int x = 10;
+                for (int row = 0; row < MAX_ROW; row++)
+                {
+                    txtBox[col, row].Visible = true;
+                    txtBox[col, row].Location = new Point(x, y);
+                    this.Controls.Add(txtBox[col, row]);
+                    x += 50;
+                }
+                y += 50;
+            }
+        }
+
+        void TxtBoxClick(object sender, EventArgs e)
+        {
+            TextBox currentBox = (sender) as TextBox;
+            if (currentBox != null)
+            {
+                if (!(currentBox.BackColor == Color.Red) && !(currentBox.BackColor == Color.Green) && !(currentBox.BackColor == Color.Orange))
+                {
+                    currentBox.BackColor = Color.Red;
+                }
+                else if (currentBox.BackColor == Color.Red)
+                {
+                    currentBox.BackColor = Color.Orange;
+                }
+                else if (currentBox.BackColor == Color.Orange)
+                {
+                    currentBox.BackColor = Color.Green;
+                }
+                else if (currentBox.BackColor == Color.Green)
+                {
+                    currentBox.BackColor = Color.Red;
+                }
+                else MessageBox.Show(Convert.ToString(currentBox.BackColor));
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
